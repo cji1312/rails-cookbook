df212956ab52cb6e31613314ad44b749a25bb64d
Charyl Idia <charyli@mailfence.com>
2024-09-30 11:04:58 +0100

    validations and associations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,2 +1,7 @@
 class Bookmark < ApplicationRecord
+  belongs_to :recipe_id
+  belongs_to :category_id
+
+  validates :recipe_id, uniqueness: { scope: category_id}
+  validates :comment, length: { minimum: 6, too_short: "must have at least 6 chars"}
 end
