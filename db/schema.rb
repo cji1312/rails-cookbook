e4e81216b56327dd0cd447d7b172b49d52e6e129
Charyl Idia <charyli@mailfence.com>
2024-09-30 10:30:11 +0100

    bookmark model

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_30_090906) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_30_090805) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "bookmarks", force: :cascade do |t|
-    t.string "comment"
-    t.integer "recipe_id"
-    t.integer "category_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
